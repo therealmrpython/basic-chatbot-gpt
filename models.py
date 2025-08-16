1eb2b8d8052e5a69e95faa25ae831fda67cdeb02
Mr Python <crispumarino@gmail.com>
2025-08-16 14:13:12 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,14 @@
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
+# load variables from file .env
+load_dotenv()
 
-client = OpenAI(api_key="***REMOVED***")
+# Get API Key from .env
+api_key = os.getenv("OPENAI_API_KEY")
 
+# Create client with the API Key
+client = OpenAI(api_key=api_key)
 
 # Listar modelos
 models = client.models.list()
